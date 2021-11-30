8fc01f2d710375fb15467a0d8509f47a4f586dda
vedantharshu <vedantharbu@gmail.com>
2021-12-01 00:59:29 +0530

    leet code tree problem sum from node until root

---
+++ SumRootToLeafNumbers.java
@@ -0,0 +1,29 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { 
+        this.val = val; 
+    }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+public class SumRootToLeafNumbers {
+    public int sumNumbers(TreeNode root) {
+        return check(root,0);
+    }
+    
+    public int check(TreeNode root, int v){
+        if(root ==  null)
+            return 0;
+        v=v*10+root.val;
+        if(root.left==null && root.right==null){
+            return v;
+        }
+        return check(root.left, v) + check(root.right, v);
+    }
+}
